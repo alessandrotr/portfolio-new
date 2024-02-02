7a97b9e812a6593c4f8b43416b66a5d42085dec4
AlessandroTraiola <a.traiola@solidwhite.de>
2024-02-02 02:15:39 +0100

    logo rotationIntensity and floatingRange

--- src/components/3d/LogoModel.tsx
+++ src/components/3d/LogoModel.tsx
@@ -28,9 +28,9 @@ const LogoModel = () => {
   return (
     <Float
       speed={2} // Animation speed, defaults to 1
-      rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+      rotationIntensity={2} // XYZ rotation intensity, defaults to 1
       floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange, defaults to 1
-      floatingRange={[-0.5, 0.5]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+      floatingRange={[-0.1, 0.1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
     >
       <Clouds material={THREE.MeshBasicMaterial}>
         <Cloud

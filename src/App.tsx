637ec27fd598efbc275cab83dc5a8ded4d0d365d
AlessandroTraiola <a.traiola@solidwhite.de>
2024-02-02 01:12:55 +0100

    changed light intensity when switching pages and on loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import ProjectsPage from "./components/pages/projects/ProjectsPage";
 import "react-tooltip/dist/react-tooltip.css";
 import { Canvas } from "@react-three/fiber";
 import LogoModel from "./components/3d/LogoModel";
+import { useSnapshot } from "valtio";
+import store from "./appStore";
 
 function App() {
+  const snap = useSnapshot(store);
+
   return (
     <>
       <Canvas
@@ -20,11 +24,23 @@ function App() {
         flat
       >
         <LogoModel />
-        <ambientLight intensity={0.085} />
+        <ambientLight
+          intensity={
+            (snap.pageActive === "HomePage" || snap.changingProject) &&
+            !snap.isLoading
+              ? 0.085
+              : 0.085
+          }
+        />
         <pointLight
           distance={50}
           position={[0, 0, 10]}
-          intensity={10}
+          intensity={
+            (snap.pageActive === "HomePage" || snap.changingProject) &&
+            !snap.isLoading
+              ? 10
+              : 2.5
+          }
           color={"#5fd9f9"}
         />
       </Canvas>
